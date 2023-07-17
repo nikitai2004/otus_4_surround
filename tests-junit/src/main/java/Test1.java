145e7bba49034cc04490fc26f0db189921f96ba2
Nikk <nikitai2004@mail.ru>
2023-07-18 02:16:20 +0400

    5

--- tests-junit/src/main/java/Test1.java
+++ tests-junit/src/main/java/Test1.java
@@ -1,12 +1,15 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.*;
-
-import static com.mysql.cj.conf.PropertyKey.logger;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Test1 {
 
     private Logger logger = LogManager.getLogger(Test1.class);
+    private WebDriver driver;
 
     @BeforeAll
     public static void beforeAll() {
@@ -15,32 +18,40 @@ public class Test1 {
 
     @BeforeEach
     public void before() {
-        logger.error("error");
-        logger.warn("warn");
-        logger.info("info");
-        logger.debug("debug");
-        System.out.println("  === Before ===");
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+
+
+//        logger.error("error");
+//        logger.warn("warn");
+//        logger.info("info");
+//        logger.debug("debug");
+//        logger.trace("trace");
+//
+//        System.out.println("  === Before ===");
     }
 
     @Test
     public void test1() {
-        Assertions.assertEquals(2, 31);
+        driver.get("https://otus.ru");
+
+        Assertions.assertEquals("Онлайн‑курсы для профессионалов, дистанционное обучение современным профессиям", driver.getTitle());
     }
 
-    @Test
-    public void test2() {
-        Assertions.assertEquals(2, 2);
-    }
-
-    @Test
-    public void test3() {
-        Assertions.assertEquals(2, 2);
-    }
-
-    @Test
-    public void test4() {
-        Assertions.assertEquals(2, 2);
-    }
+//    @Test
+//    public void test2() {
+//        Assertions.assertEquals(2, 2);
+//    }
+//
+//    @Test
+//    public void test3() {
+//        Assertions.assertEquals(2, 2);
+//    }
+//
+//    @Test
+//    public void test4() {
+//        Assertions.assertEquals(2, 2);
+//    }
 
 
     @AfterEach
