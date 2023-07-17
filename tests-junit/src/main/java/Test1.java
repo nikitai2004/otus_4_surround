a2274848ef288515564c265f17c82ccfda90a68d
Nikk <nikitai2004@mail.ru>
2023-07-18 01:47:22 +0400

    4

--- tests-junit/src/main/java/Test1.java
+++ tests-junit/src/main/java/Test1.java
@@ -4,54 +4,52 @@ import org.junit.jupiter.api.*;
 
 import static com.mysql.cj.conf.PropertyKey.logger;
 
-public class Test1{
+public class Test1 {
 
-    private Logger loggger = LogManager.getLogger(Test1.class);
+    private Logger logger = LogManager.getLogger(Test1.class);
 
     @BeforeAll
-    public static void beforeAll(){
+    public static void beforeAll() {
         System.out.println("  === BeforeAll ===");
     }
 
     @BeforeEach
-    public void before(){
+    public void before() {
+        logger.error("error");
         logger.warn("warn");
         logger.info("info");
         logger.debug("debug");
-        logger.info("info");
-
-
         System.out.println("  === Before ===");
     }
 
     @Test
-    public void test1(){
-        Assertions.assertEquals(2,31);
+    public void test1() {
+        Assertions.assertEquals(2, 31);
     }
 
     @Test
-    public void test2(){
-        Assertions.assertEquals(2,2);
+    public void test2() {
+        Assertions.assertEquals(2, 2);
     }
 
     @Test
-    public void test3(){
-        Assertions.assertEquals(2,2);
+    public void test3() {
+        Assertions.assertEquals(2, 2);
     }
 
     @Test
-    public void test4(){
-        Assertions.assertEquals(2,2);
+    public void test4() {
+        Assertions.assertEquals(2, 2);
     }
 
 
     @AfterEach
-    public void closeAll (){
+    public void closeAll() {
         System.out.println("  === AfterEach ===");
     }
 
     @AfterAll
-    public static void endAll (){
+    public static void endAll() {
         System.out.println("  === AfterAll ===");
     }
 
